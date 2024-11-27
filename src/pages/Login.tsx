76606d250274655d68457aad019c41f8479707b7
AndreOD <115731734+AndreOD@users.noreply.github.com>
2024-11-27 20:37:10 +0000

    package fixes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>("");
